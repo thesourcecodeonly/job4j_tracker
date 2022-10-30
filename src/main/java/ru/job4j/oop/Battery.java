d11a23d2d232601a92d1de86042a84a5aad49f17
vyacheslav833 <zv.specialone@gmail.com>
2022-10-30 15:14:39 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery batteryFirst = new Battery(50);
+        Battery batterySecond = new Battery(30);
+        System.out.println("batteryFirst: " + batteryFirst.load + ". batterySecond: "
+                + batterySecond.load);
+        batterySecond.exchange(batteryFirst);
+        System.out.println("batteryFirst: " + batteryFirst.load + ". batterySecond: "
+                + batterySecond.load);
+    }
+}
