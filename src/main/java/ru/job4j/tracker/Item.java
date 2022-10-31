371ab88b2bdfe42ec7bf571e934dc435932c2727
vyacheslav833 <zv.specialone@gmail.com>
2022-10-31 16:58:44 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,18 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +30,11 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item item1 = new Item();
+        Item item2 = new Item("Viktor");
+        Item item3 = new Item(33, "Vika");
     }
 }
