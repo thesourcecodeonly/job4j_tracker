f7aaa155efc3b1ae9d5fc4e5ef12e4d39c6add12
vyacheslav833 <zv.specialone@gmail.com>
2022-11-10 12:32:52 +0300

    0.4. Пользовательские исключения. [#219367 #359578] - corrected

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -5,8 +5,9 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int index = 0; index < value.length; index++) {
-            if (value[index] == key) {
+            if (value[index].equals(key)) {
                 rsl = index;
+                break;
             }
         }
         if (rsl == -1) {
@@ -16,7 +17,7 @@ public class FindEl {
     }
 
     public static void main(String[] args) {
-        String[] arr = {"qwe", "rty"};
+        String[] arr = {"qwe", "rty", "uio"};
         String key = "rt";
         try {
             indexOf(arr, key);
