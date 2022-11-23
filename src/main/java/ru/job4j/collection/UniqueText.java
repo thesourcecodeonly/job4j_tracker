36a0444b99e2c0af33c4914e77d0ba666a1f8e97
vyacheslav833 <zv.specialone@gmail.com>
2022-11-23 08:21:47 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String s : origin) {
+            check.add(s);
+        }
+        for (String str : text) {
+            if (!check.contains(str)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
