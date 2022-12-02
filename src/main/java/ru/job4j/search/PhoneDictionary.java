40c5cbe0d23510e58836ba3362f99c5a967b6054
vyacheslav833 <zv.specialone@gmail.com>
2022-12-02 17:15:42 +0300

    7. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
 
@@ -11,26 +12,18 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
+        Predicate<Person> compareName = cn -> cn.getName().contains(key);
+        Predicate<Person> compareSurname = cs -> cs.getSurname().contains(key);
+        Predicate<Person> comparePhone = cp -> cp.getPhone().contains(key);
+        Predicate<Person> compareAddress = ca -> ca.getAddress().contains(key);
+        Predicate<Person> combine = compareName.or(compareSurname)
+                .or(comparePhone).or(compareAddress);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key) || person.getSurname().contains(key)
-                    || person.getPhone().contains(key) || person.getAddress().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
-
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        PhoneDictionary phones = new PhoneDictionary();
-        phones.add(
-                new Person("Petr", "Arsentev", "534872", "Bryansk")
-        );
-        phones.add(
-                new Person("Ivan", "Ilyshin", "534351", "Moscow")
-        );
-        ArrayList<Person> persons = phones.find("qwerty");
-        System.out.println(persons.isEmpty());
     }
 }
